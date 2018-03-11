07390adfd093c89f40ba395d23abba5cb3d8224a
Evan Xu <evanxu1987@gmail.com>
2018-03-11 20:16:52 +0800

    edit style

--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -166,7 +166,7 @@ Page({
     wx.getSystemInfo({
       success: function (res) {
         that.setData({
-          sliderLeft: (res.windowWidth / that.data.tabs.length - res.windowWidth / 2) / 2,
+          sliderLeft: (res.windowWidth / that.data.tabs.length - res.windowWidth /2) / 2,
           sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
         });
       }
